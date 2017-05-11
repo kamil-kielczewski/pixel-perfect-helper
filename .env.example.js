ca054880d118ab9c7aa0256435df1109ce067fcc
Kamil Kiełczewski <kamil.k.kielczewski@gmail.com>
2017-05-11 08:38:55 +0200

    add: .env e.g.

--- .env.example.js
+++ .env.example.js
@@ -2,8 +2,8 @@
 
 var EnvData = {
     // If some of below variable is NULL then that varabile feature is off
-    google_analaytics : null,           //'XX-YYYYYYYY-Z',
-    sentry_clientKey_publicDSN: null,   //'https://xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx@sentry.io/yyyyyy',
+    google_analaytics : null,           // e.g. 'XX-YYYYYYYY-Z',
+    sentry_clientKey_publicDSN: null,   //'e.g. https://xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx@sentry.io/yyyyyy',
 };
 
 module.exports = EnvData;
