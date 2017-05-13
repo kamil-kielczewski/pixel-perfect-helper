8f3b15d3ab1b5cb0c9efb3cbba70051aa69386af
Kamil Kiełczewski <kamil.k.kielczewski@gmail.com>
2017-05-13 12:32:19 +0200

    fix: put null to google_analytics

--- .env.example.js
+++ .env.example.js
@@ -2,7 +2,7 @@
 
 var EnvData = {
     // If some of below variable is NULL then that varabile feature is off
-    google_analaytics : 'ffff',           // e.g. 'XX-YYYYYYYY-Z',
+    google_analaytics : null,           // e.g. 'XX-YYYYYYYY-Z',
     sentry_clientKey_publicDSN: null,   //'e.g. https://xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx@sentry.io/yyyyyy',
 };
 
