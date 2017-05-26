56ea9fa38fb4205ce112fdfc568ae2ccc156b53b
Kamil Kiełczewski <kamil.k.kielczewski@gmail.com>
2017-05-26 11:49:59 +0200

    doc: change comment

--- .env.example.js
+++ .env.example.js
@@ -1,7 +1,7 @@
 // copy this file to './.env.js' and modify variables in EnvData
 
+// If some of below variable is NULL then that varabile feature is off
 var EnvData = {
-    // If some of below variable is NULL then that varabile feature is off
     google_analaytics : null,           // e.g. 'XX-YYYYYYYY-Z',
     sentry_clientKey_publicDSN: null,   //'e.g. https://xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx@sentry.io/yyyyyy',
 };
