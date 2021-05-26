0de8ce24350615d0f1c053cb593e2bcde4011d65
Kamil Kiełczewski <kamil.k.kielczewski@gmail.com>
2021-05-26 19:08:45 +0200

    ref: add dist

--- .env.example.js
+++
@@ -1,9 +0,0 @@
-// copy this file to './.env.js' and modify variables in EnvData
-
-// If some of below variable is NULL then that varabile feature is off
-var EnvData = {
-    google_analaytics : null,           // e.g. 'XX-YYYYYYYY-Z',
-    sentry_clientKey_publicDSN: null,   //'e.g. https://xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx@sentry.io/yyyyyy',
-};
-
-module.exports = EnvData;
